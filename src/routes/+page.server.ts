d68e3dd006ce89e091217c2841777c1aad470803
RedHellier <redhellier@gmail.com>
2025-04-24 10:21:03 +0100

    dummy-netlify

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+export async function load() {
+
+    const response = await fetch('http://localhost:5000/');
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+    const text: string = await response.text();
+
+	return {
+		entries: ['data', 'data2', text]
+	};
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+    interface Props {
+        data: {
+            entries: string[];
+        }
+    }
+
+	const { data }: Props = $props();
+    
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+{#each data.entries as entry}
+	<h1>{entry}</h1>
+{/each}
