7210f8c383cca0b1c9372e18e8694b9a609461f7
RedHellier <redhellier@gmail.com>
2025-04-24 11:01:37 +0100

    removed fetch

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,11 @@
 export async function load() {
-
-    const response = await fetch('http://localhost:5000/');
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
-        }
-    const text: string = await response.text();
+	// const response = await fetch('http://localhost:5000/');
+	//     if (!response.ok) {
+	//         throw new Error('Failed to fetch data');
+	//     }
+	// const text: string = await response.text();
 
 	return {
-		entries: ['data', 'data2', text]
+		entries: ['data', 'data2', 'data3']
 	};
 }
